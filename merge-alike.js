b2fe36ad0012d096656e093ecb3e5f0fb407b017
vinegar-tom <61390579+vinegar-tom@users.noreply.github.com>
2021-07-20 09:53:21 -0500

    Update merge-alike.js

--- merge-alike.js
+++ merge-alike.js
@@ -1,6 +1,7 @@
 function mergeAlike() {
   const sheet = SpreadsheetApp.getActiveSheet();
   const primaryIDs = sheet.getRange(2, 9, sheet.getLastRow() - 1).getValues();
+
   var rowCount = 0;
   const alikeRows = [];
 
@@ -12,21 +13,16 @@ function mergeAlike() {
         rowCount = 0;
       }
     } else {
-      if (primaryIDs[i][0] !== primaryIDs[i - 1][0]) {
-        alikeRows.push(1);
-      } else {
-        rowCount += 1;
-        alikeRows.push(rowCount);
-      }
+      rowCount += 1;
+      alikeRows.push(rowCount);
     }
   }
 
-  rowCount = 2;
+  var row = 2;
 
-  for (let i = 0; i < alikeRows.length; i++) {
-    const mergeNumber = alikeRows[i];
-    sheet.getRange(rowCount, 12, mergeNumber).merge();
-    sheet.getRange(rowCount, 13, mergeNumber).merge();
-    rowCount += mergeNumber;
+  for (const number of alikeRows) {
+    sheet.getRange(row, 12, number).merge();
+    sheet.getRange(row, 13, number).merge();
+    row += number;
   }
 }

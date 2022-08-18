1f7f62669e43e30557e874196f57be7c13c0b692
vinegar-tom <61390579+vinegar-tom@users.noreply.github.com>
2022-08-18 15:37:10 -0500

    Normalize characters to separate diacritics

--- efs-remove-special-chars.js
+++ efs-remove-special-chars.js
@@ -8,7 +8,7 @@ function removeSpecialChars() {
   const ampersand = /&/gu;
 
   for (let i = 0; i < lineNotes.length; i++) {
-    let lineNote = lineNotes[i][0];
+    let lineNote = lineNotes[i][0].normalize("NFKD");
 
     lineNote = lineNote.replaceAll(nonASCII, '');
     lineNote = lineNote.replaceAll(otherIllegal, '');
